31bc53828e4edfa6c858a6edddd7a03b43e028be
kushal chatterjee <139739244+chatterjeekushal@users.noreply.github.com>
2023-09-02 21:14:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,129 @@
+
+
+
+let tag=document.querySelector(".navbtn");
+let show=document.querySelector(".popupwraper")
+
+
+    
+        
+
+
+
+let close=document.querySelector(".close")
+
+
+
+
+
+tag.addEventListener('click',function(){
+
+    show.classList.add("opacity-100");
+});
+
+close.addEventListener('click',function(){
+
+show.classList.remove("opacity-100");
+
+});
+
+
+
+
+
+// for responsiv navbar
+
+let hambargar=document.querySelector(".hambargar");
+let resheader=document.querySelector(".resheader");
+
+let resclose=document.querySelector(".resclose");
+
+
+hambargar.addEventListener('click',function(){
+
+    resheader.classList.remove("hidden");
+})
+
+resclose.addEventListener('click',function(){
+
+    resheader.classList.add("hidden");
+
+})
+
+// form varification
+
+let go = false
+
+
+
+
+
+
+
+
+function chak() {
+
+
+
+  let user = document.myform.user.value;
+
+  let useralert = document.querySelector(".alert");
+
+
+  let email = document.myform.email.value;
+  let emailalert = document.querySelector("#emailalert");
+
+
+  let chakbox = document.myform.chakbox;
+
+  let chakalert = document.querySelector("#chakalert");
+
+
+
+  if (user == "") {
+
+    useralert.classList.remove("d-none");
+    return false
+
+  }
+
+
+  else {
+
+    useralert.classList.add("d-none");
+  }
+
+  if (email == "") {
+
+    emailalert.classList.remove("d-none");
+    return false;
+  }
+
+  else {
+
+    emailalert.classList.add("d-none");
+  }
+
+
+  if (chakbox.checked == "") {
+
+    chakalert.classList.remove("d-none");
+    return false;
+
+  }
+
+  else {
+
+    chakalert.classList.add("d-none");
+  }
+
+
+
+}
+
+
+
+
+
+
+
